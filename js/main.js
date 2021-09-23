73b918d81bff6cd14a2812c4b69aeda8cf1c86b1
awmills25 <awmills25@gmail.com>
2021-09-23 17:47:09 -0400

    added js comments

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+//start on first page
 let currentPage = 0;
 
+//create array full of 100 symbols
 var symbol1 = ["!", "@", "#", "$", "%", "&", "A", "H", "~", "^", "!", "@", "#", "$", "%", "&", "A", "H", "~", "^", "!", "@", "#", "$", "%", "&", "A", "H", "~", "^", "!", "@", "#", "$", "%", "&", "A", "H", "~", "^", "!", "@", "#", "$", "%", "&", "A", "H", "~", "^"];
 var symbol2 = ["!", "@", "#", "$", "%", "&", "A", "H", "~", "^", "!", "@", "#", "$", "%", "&", "A", "H", "~", "^", "!", "@", "#", "$", "%", "&", "A", "H", "~", "^", "!", "@", "#", "$", "%", "&", "A", "H", "~", "^", "!", "@", "#", "$", "%", "&", "A", "H", "~", "^"];
 var symbol = symbol1.concat(symbol2);
 
+//empty int variable that will show the random symbol that reads users mind
 var yourSym = null;
 
+//create variables and link them to html
 var bigText = document.getElementById("bigText");
 var helperText = document.getElementById("helperText");
 var actionBtn = document.getElementById("actionBtn");
@@ -13,28 +17,36 @@ var revBtn = document.getElementById("revBtn");
 var resetBtn = document.getElementById("resetBtn");
 var startBtn = document.getElementById("startBtn");
 
+//function that sends us to the next page and runs state change
 function actionBtnMethod() {
     currentPage++;
     onStateChange();
 }
+//on click event listeners for each button sending to next page
 actionBtn.addEventListener('click', actionBtnMethod);
 startBtn.addEventListener('click', actionBtnMethod);
 revBtn.addEventListener('click', actionBtnMethod);
 
+//function to send us back to first page
 function resetBtnMethod() {
     currentPage = 0;
     onStateChange();
 }
+//on click event listener for reset button
 resetBtn.addEventListener('click', resetBtnMethod);
 
+//function to hide elements
 function hide(element) {
     element.style.visibility = "hidden";
 }
 
+//function to show elements
 function show(element) {
     element.style.visibility = "visible";
 }
 
+//function that creates a random symbol and returns it as a string
+//also sets our symbol for everything divisible by 9
 function randomSym() {
     var symString = "";
     var randomNine = symbol[Math.floor(Math.random() * symbol.length)];
@@ -52,8 +64,9 @@ function randomSym() {
     return symString;
 }
 
+//state management function
+//switch case for each page and the elements seen on each
 function onStateChange() {
-    console.log(currentPage);
     switch (currentPage) {
         case 0:
             bigText.innerHTML="I can read your mind";
@@ -64,7 +77,7 @@ function onStateChange() {
             show(startBtn);
             break;
         case 1:
-            bigText.innerHTML="Pick a number from 01-99";
+            bigText.innerHTML="Pick a number from 01 - 99";
             show(actionBtn);
             hide(revBtn);
             show(helperText);
@@ -111,6 +124,7 @@ function onStateChange() {
     }
 }
 
+//function that runs and pulls up when the page is loaded
 function init(){
     resetBtnMethod();
     onStateChange();
